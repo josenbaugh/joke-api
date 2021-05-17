171df0069241f5b2c353fb6fc354b55f54237317
Joshua Osenbaugh <josh@osenbaugh.xyz>
2021-05-17 01:29:27 -0400

    catch exceptions in app controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,18 +4,25 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Psr\Log\LoggerInterface;
 use FOS\RestBundle\Controller\Annotations\Get;
+use App\Service\AppService;
+use App\Exception\VendorApiException;
 
 class AppController
 {
     /**
      * @Get("/joke/random", name="get_random_joke")
      */
-    public function number(LoggerInterface $logger): JsonResponse
+    public function number(LoggerInterface $logger, AppService $service): JsonResponse
     {
-		$logger->info('test');
-        return new JsonResponse(['success'=>true], Response::HTTP_OK);
+		try {
+			$data = $service->getJoke();
+			return new JsonResponse(['success'=>true, 'data'=>$data], Response::HTTP_OK);
+		}
+		catch (VendorApiException | \Exception  $e) {
+			$logger->error($e->getMessage());
+			return new JsonResponse(['success'=>false, 'data'=>'Exception occured please contact server admin.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+		}
     }
 }
